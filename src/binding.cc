d6f79e72b97b38d7ce18e846d1a3e27b72b5e27f
Ned Palacios <npdoesmc@gmail.com>
2020-01-19 17:56:53 +0800

    fix builds on node-gyp

--- src/binding.cc
+++ src/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_V();
+extern "C" TSLanguage * tree_sitter_v();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_V());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_v());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("V").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("v").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_V_binding, Init)
+NODE_MODULE(tree_sitter_v_binding, Init)
 
 }  // namespace
