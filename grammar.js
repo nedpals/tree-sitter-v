bf7843a394cd9d01aacc0ee963c870a933da8bab
Ned Palacios <npdoesmc@gmail.com>
2020-04-05 09:08:13 +0800

    fn_attribute: do not specify list of valid attributes

--- grammar.js
+++ grammar.js
@@ -198,7 +198,7 @@ module.exports = grammar({
 
     fn_attribute: $ => seq(
       '[',
-      choice('if', 'deprecated', 'inline', 'typedef',),
+      field('name', $.identifier),
       ']'
     ),
 
