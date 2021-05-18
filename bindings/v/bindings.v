ac6dccda9d2e5426606f3f7692c13c2f77b6581d
Ned Palacios <npdoesmc@gmail.com>
2021-05-18 15:57:10 +0800

    bindings/v: add load_node_types fn

--- bindings/v/bindings.v
+++ bindings/v/bindings.v
@@ -3,6 +3,16 @@ module v
 #flag -I @VMODROOT/src
 #flag @VMODROOT/src/parser.o
 
+import x.json2
+import os
+
 fn C.tree_sitter_v() &C.TSLanguage
 
 pub const language = unsafe { C.tree_sitter_v() }
+
+// load_node_types reads the node-types.json file for static analysis.
+pub fn load_node_types() ?[]json2.Any {
+	contents := os.read_file(os.join_path(@VMODROOT, 'src', 'node-types.json')) ?
+	list := json2.raw_decode(contents) ?
+	return list.as_arr()
+}
